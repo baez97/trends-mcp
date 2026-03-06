5073049dfb4ca9b1e142246307b9334f4a764ce0
Manu Baez <jmbaez@google.com>
2026-03-06 15:31:34 +0100

    Third commit

--- server.py
+++ server.py
@@ -6,13 +6,12 @@ mcp = FastMCP("Email MCP Server 📨")
 @mcp.tool
 def get_google_trends():
     """
-    Returns a list of Google Trends related to SaaS
+    Returns a list of Google Trends related to Software
     """
     return ["AI Agent", "n8n", "Blockchain"]
 
-if __name__ == "__main__":
-    mcp.run(
-        transport="http",
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8080))
-    )
+mcp.run(
+    transport="http",
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", 8080))
+)
